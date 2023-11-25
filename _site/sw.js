12f7d1685e136435c0304b6d1f12d6868dfab2f7
SeJinLim9577 <goodoo9577@gmail.com>
2023-11-25 23:50:58 +0900

    test push

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1700923333';
+const cacheName = 'chirpy-1700923827';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
